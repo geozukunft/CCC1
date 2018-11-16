903eaf5e0577da3eaac08cc76a07bb5e7c48c6c3
NemeDa <40146720160278@litec.ac.at>
2018-11-16 15:59:37 +0100

    dsf

--- CCC29/Program.cs
+++ CCC29/Program.cs
@@ -9,43 +9,51 @@ namespace CCC_29
 {
     class Program
     {
-        static string[] InString = new string[10000];
-        static int[,] Output = new int[3000, 3000];
+        static int[] Daten = new int[1000];
 
         static void Main(string[] args)
-        {
-            Einlesen();
-
-
-        }
-
-        static public int[,] Einlesen(string path)
         {
             string Input;
 
 
-            StreamReader reader = new StreamReader(path);
 
-            Input = reader.ReadToEnd();
-            reader.Close();
 
-            InString = Input.Split('\n');
 
-            for (int i = 1; InString[i + 1] != null; i++)
+
+
+
+
+            
+            int i = 0;
+            while(i < 5)
             {
-                string s = InString[i];
 
-                string[] s1 = new string[1000];
+            StreamReader reader = new StreamReader(readerpath);
+            StreamWriter writer = new StreamWriter(writerpath);
+            while ((Input = reader.ReadLine()) != null)
+            {
 
-                s1 = s.Split(' ');
+                string[] input = Input.Split(' ');
+                int r1 = Convert.ToInt32(input[0]);
+                int r2 = Convert.ToInt32(input[3]);
+                int c1 = Convert.ToInt32(input[1]);
+                int c2 = Convert.ToInt32(input[4]);
+                double ratio = Convert.ToDouble(input[5]);
+                int x = Convert.ToInt32(r1 + ratio * (r2 - r1));
+                int y = Convert.ToInt32(c1 + ratio * (c2 - c1));
 
-                for (int j = 0; s1[j + 1] != null; j++)
-                {
-                    Output[i, j] = Convert.ToInt16(s1[j]);
-                }
+                writer.WriteLine($"{x} {y}");
+
             }
+            }
+            Console.ReadKey();
+        }
 
-            return Output;
+        static public Einlesen(string path)
+        {
+            
+
+                
         }
     }
 }
